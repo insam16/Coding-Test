9a99290ba117b8c924da4841892adc9b1bed2dc0
insam16 <53588466+insam16@users.noreply.github.com>
2023-11-05 23:45:29 +0900

    Update calc_str.cs

--- calc_str.cs
+++ calc_str.cs
@@ -1,13 +1,30 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120902?language=csharp
+
 using System;
 
 public class Solution
 {
     public int solution(string my_string)
     {
-        int answer = 0; string[] words = my_string.Split(' ');
+        int answer = 0;
+        string[] strs = my_string.Split(' ');
+        bool op;
 
-
-
+        foreach (string str in strs)
+        {
+            if (str == "+")
+            {
+                op = true;
+            }
+            else if (str == "-")
+            {
+                op = false;
+            }
+            else //number
+            {
+                answer = (op ? answer + int.Parse(str) : answer - int.Parse(str));
+            }
+        }
         return answer;
     }
 }
