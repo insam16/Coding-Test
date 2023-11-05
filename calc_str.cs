2bccfec0f89ad30513831c2f2c6030f050c7bb01
insam16 <53588466+insam16@users.noreply.github.com>
2023-11-05 23:36:31 +0900

    Add files via upload

---
+++ calc_str.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    public int solution(string my_string)
+    {
+        int answer = 0; string[] words = my_string.Split(' ');
+
+
+
+        return answer;
+    }
+}
