e71805a28fb7191e5642ab1e721710b0eacd65dd
insam16 <53588466+insam16@users.noreply.github.com>
2023-11-12 23:15:02 +0900

    commit Day 5 problems

---
+++ reverse_array.cs
@@ -0,0 +1,12 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120821?language=csharp
+
+using System;
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        Array.Reverse(num_list);
+        return num_list;
+    }
+}
