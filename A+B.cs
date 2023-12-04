8a2ec7f329623932ed6eb3c085c3321bcdcbe10a
insam16 <yumin5541@naver.com>
2023-12-05 02:21:47 +0900

    [Baekjoon-1000] A+B

---
+++ A+B.cs
@@ -0,0 +1,12 @@
+﻿//https://www.acmicpc.net/problem/1000
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        Console.WriteLine(Int32.Parse(input[0]) + Int32.Parse(input[1]));
+    }
+}
