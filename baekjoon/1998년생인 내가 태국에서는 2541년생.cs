bcc96706a8ef34c36aae52f2f0631b640eda7708
insam16 <yumin5541@naver.com>
2023-12-08 17:22:07 +0900

    [Baekjoon-18108] 1998년생인 내가 태국에서는 2541년생?!

---
+++ baekjoon/1998년생인 내가 태국에서는 2541년생.cs
@@ -0,0 +1,11 @@
+//https://www.acmicpc.net/problem/18108
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Int32.Parse(Console.ReadLine()) - 543);
+    }
+}
