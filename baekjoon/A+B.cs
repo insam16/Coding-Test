63842232f48a1cebc5f82dda9a7c35d4503a2ac1
insam16 <yumin5541@naver.com>
2023-12-08 17:40:44 +0900

    [Baekjoon-1000] A+B

--- baekjoon/A+B.cs
+++ baekjoon/A+B.cs
@@ -6,7 +6,17 @@ class Program
 {
     static void Main()
     {
+        // 1.
+        /*
         string[] input = Console.ReadLine().Split(' ');
         Console.WriteLine(Int32.Parse(input[0]) + Int32.Parse(input[1]));
+        */
+
+        // 2.
+        // ascii code
+        // " ": 32
+        // "0": 48
+        // 128 = 48 + 32 + 48
+        Console.WriteLine(Console.Read() + Console.Read() + Console.Read() - 128);
     }
 }
