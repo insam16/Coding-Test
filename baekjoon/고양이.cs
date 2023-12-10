f2440d0b1c2f11ef186aefe7b1106c23eb626fea
insam16 <yumin5541@naver.com>
2023-12-10 22:11:55 +0900

    [Baekjoon-10171] 고양이

---
+++ baekjoon/고양이.cs
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/10171
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
