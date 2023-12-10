86dfd5e493ff38640bd05f3474845f9e2ea12166
insam16 <yumin5541@naver.com>
2023-12-10 22:30:33 +0900

    [Baekjoon-10172] 개

---
+++ baekjoon/개.cs
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/10172
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
