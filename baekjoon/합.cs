700b5b024e1c3aee207d8a610e0f330807f605c5
insam16 <yumin5541@naver.com>
2023-12-12 10:27:47 +0900

    [Baekjoon-8393] 합

---
+++ baekjoon/합.cs
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/8393
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine( (n+1) * n/2 );
+    }
+}
