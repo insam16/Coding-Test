7c1e658afb0073a63cbf291813b9b997943e127f
insam16 <yumin5541@naver.com>
2023-12-20 08:54:17 +0900

    [Baekjoon-10807] 개수 세기 ver.2

--- baekjoon/X보다 작은 수.cs
+++ baekjoon/X보다 작은 수.cs
@@ -1,5 +1,9 @@
 //https://www.acmicpc.net/problem/10871
 
+// =========================================================
+// 1. StringBuilder
+// =========================================================
+/*
 using System;
 using System.Text;
 
@@ -23,5 +27,34 @@ class Program
         }
 
         Console.WriteLine(sb);
+    }
+}
+*/
+
+// =========================================================
+// 2. StreamWriter
+// =========================================================
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int number = int.Parse(input[0]);
+        int X = int.Parse(input[1]);
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        input = Console.ReadLine().Split(" ");
+
+        for (int i = 0; i < number; ++i)
+        {
+            if (int.Parse(input[i]) < X)
+            {
+                sw.Write(input[i] + " ");
+            }
+        }
+        sw.Close();
     }
 }
