fa79d6630a527f30afddbb0327e3f102c9fbfa5c
insam16 <yumin5541@naver.com>
2023-12-25 23:48:23 +0900

    [Baekjoon-1546] 평균

---
+++ baekjoon/평균.cs
@@ -0,0 +1,32 @@
+//https://www.acmicpc.net/problem/1546
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        double max = 0;
+        int subjects = int.Parse(Console.ReadLine());
+        int[] scores = new int[subjects];
+        double sum = 0;
+        string[] input = Console.ReadLine().Split(" ");
+
+        for (int i = 0; i < subjects; ++i)
+        {
+            scores[i] = int.Parse(input[i]);
+            if (scores[i] > max) max = (double)scores[i];
+        }
+
+        for (int i = 0; i < subjects; ++i)
+        {
+            sum += scores[i] / max * 100;
+        }
+
+        sw.WriteLine(sum / subjects);
+        sw.Close();
+    }
+}
