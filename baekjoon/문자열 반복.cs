6aaa80072805f8accf1f95f1bb8f362ed01666cb
insam16 <yumin5541@naver.com>
2023-12-29 08:55:58 +0900

    [Baekjoon-2675] 문자열 반복

---
+++ baekjoon/문자열 반복.cs
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/10809
+
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int testcase = int.Parse(Console.ReadLine());
+        
+        string[] input;
+        int repeat;
+        string str;
+
+        for (int i = 0; i < testcase; ++i)
+        {
+            input = Console.ReadLine().Split(" ");
+            repeat = int.Parse(input[0]);
+            str = input[1];
+
+            for (int j = 0; j < str.Length; ++j)
+            {
+                sw.Write(new string(str[j], repeat));
+            }
+            sw.WriteLine();
+        }
+        sw.Close();
+    }
+}
