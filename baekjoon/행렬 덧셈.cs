558239f820fc5897ee33bda7fd3a5651cdbc9907
insam16 <yumin5541@naver.com>
2024-01-08 17:00:41 +0900

    [Baekjoon-2738] 행렬 덧셈

---
+++ baekjoon/행렬 덧셈.cs
@@ -0,0 +1,48 @@
+//https://www.acmicpc.net/problem/2738
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = Console.ReadLine().Split();
+        int size_N = int.Parse(input[0]);
+        int size_M = int.Parse(input[1]);
+
+        int[,] sum = new int[size_N, size_M];
+
+        for (int i = 0; i < size_N; i++)
+        {
+            input = Console.ReadLine().Split();
+
+            for (int j = 0; j < size_M; j++)
+            {
+                sum[i, j] = int.Parse(input[j]);
+            }
+        }
+
+        for (int i = 0; i < size_N; i++)
+        {
+            input = Console.ReadLine().Split();
+
+            for (int j = 0; j < size_M; j++)
+            {
+                sum[i, j] += int.Parse(input[j]);
+            }
+        }
+
+        for (int i = 0; i < size_N; i++)
+        {
+            for (int j = 0; j < size_M; j++)
+            {
+                sw.Write(sum[i, j] + " ");
+            }
+            sw.WriteLine();
+        }
+        sw.Close();
+    }
+}
