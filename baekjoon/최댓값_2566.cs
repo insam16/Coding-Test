395f68fbec77287632a7d01ceb1a83f1850ffb33
insam16 <yumin5541@naver.com>
2024-01-09 08:19:45 +0900

    [Baekjoon-2566] 최댓값

---
+++ baekjoon/최댓값_2566.cs
@@ -0,0 +1,37 @@
+//https://www.acmicpc.net/problem/2566
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input;
+        int[,] sum = new int[9, 9];
+        int max = 0;
+        int max_row = 0;
+        int max_col = 0;
+
+        for (int i = 0; i < 9; ++i)
+        {
+            input = Console.ReadLine().Split();
+
+            for (int j = 0; j < 9; ++j)
+            {
+                sum[i, j] = int.Parse(input[j]);
+
+                if (sum[i, j] > max)
+                {
+                    max = sum[i, j];
+                    max_row = i + 1;
+                    max_col = j + 1;
+                }
+            }
+        }
+        sw.WriteLine(max + "\n" + max_row + " " + max_col);
+        sw.Close();
+    }
+}
